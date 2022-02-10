542326f7b41b5a50adee71acec8ab2c18a05dad6
howmun <howmun@gmail.com>
2022-02-10 22:04:53 +0800

    create index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+addEventListener("fetch", (event) => {
+  event.respondWith(
+    handleRequest(event.request).catch(
+      (err) => new Response(err.stack, { status: 500 })
+    )
+  );
+});
+
+/**
+ * Many more examples available at:
+ *   https://developers.cloudflare.com/workers/examples
+ * @param {Request} request
+ * @returns {Promise<Response>}
+ */
+async function handleRequest(request) {
+  const { pathname } = new URL(request.url);
+
+  if (pathname.startsWith("/api")) {
+    return new Response(JSON.stringify({ pathname }), {
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
+  if (pathname.startsWith("/status")) {
+    const httpStatusCode = Number(pathname.split("/")[2]);
+
+    return Number.isInteger(httpStatusCode)
+      ? fetch("https://http.cat/" + httpStatusCode)
+      : new Response("That's not a valid HTTP status code.");
+  }
+
+  const clientIP = request.headers.get("CF-Connecting-IP");
+
+  return new Response(clientIP, { status: 200 });
+}
